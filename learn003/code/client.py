97ecd44e6f7b99b6f1caf15bd1746a96918f8701
Himenon <k.himeno314@gmail.com>
2018-05-15 23:42:20 +0900

    add linter

--- learn003/code/client.py
+++ learn003/code/client.py
@@ -1,20 +1,18 @@
 import requests
 
+
 def register_task(task_id):
-    params = {
-      'task_id': task_id
-    }
-    res = requests.get('http://localhost:5000/task', params=params)
-    print(res.json())
+    params = {'task_id': task_id}
+    requests.get('http://localhost:5000/task', params=params)
+
 
 def get_result(task_id):
-    params = {
-      'task_id': task_id
-    }
+    params = {'task_id': task_id}
     res = requests.get('http://localhost:5000/result', params=params)
     if res.status_code == 200:
         return res.json()
     print(res.text)
+
 
 if __name__ == '__main__':
     register_task(1)
